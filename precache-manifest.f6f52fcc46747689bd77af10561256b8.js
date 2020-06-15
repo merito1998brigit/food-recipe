26f13c4cf8737c24cb5f311491e77a64134a9ce1
merito <brigit87@gmail.com>
2020-06-16 02:03:34 +0530

    Updates

---
+++ precache-manifest.f6f52fcc46747689bd77af10561256b8.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283026bbcc81739e9f65a650e80025b9",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "67327cf411a7707d8909",
+    "url": "/food-recipe/static/css/2.7f7b25bd.chunk.css"
+  },
+  {
+    "revision": "72c67818831c1eded8a9",
+    "url": "/food-recipe/static/css/main.0abc3701.chunk.css"
+  },
+  {
+    "revision": "67327cf411a7707d8909",
+    "url": "/food-recipe/static/js/2.cc3aff78.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/food-recipe/static/js/2.cc3aff78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72c67818831c1eded8a9",
+    "url": "/food-recipe/static/js/main.7bdf22eb.chunk.js"
+  },
+  {
+    "revision": "c6ab97473ac8c3a9e63e",
+    "url": "/food-recipe/static/js/runtime-main.49f9bbfe.js"
+  },
+  {
+    "revision": "319d3688217b8704842e82af959d9abe",
+    "url": "/food-recipe/static/media/alooparatha.319d3688.JPG"
+  },
+  {
+    "revision": "b68e855519ce10625a0dbccc88563545",
+    "url": "/food-recipe/static/media/background1.b68e8555.jpg"
+  },
+  {
+    "revision": "e365d91311105c0e60a948cedf6cfd94",
+    "url": "/food-recipe/static/media/butterchicken.e365d913.JPG"
+  },
+  {
+    "revision": "3df38051c40165a6cef97224b69fc91d",
+    "url": "/food-recipe/static/media/chettinadu.3df38051.JPG"
+  },
+  {
+    "revision": "8559f84475b4705bfbd7c3f0666bc096",
+    "url": "/food-recipe/static/media/chickenmarsala.8559f844.JPG"
+  },
+  {
+    "revision": "09358b8cd517dc5524e55a3e5de1f71d",
+    "url": "/food-recipe/static/media/chinese (2).09358b8c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/food-recipe/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/food-recipe/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/food-recipe/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/food-recipe/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/food-recipe/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "90a02d1e244c4fd61aa2a74b0847a13a",
+    "url": "/food-recipe/static/media/garlic bread.90a02d1e.JPG"
+  },
+  {
+    "revision": "ce742601d33f27ac983047b3a76de6ec",
+    "url": "/food-recipe/static/media/gheeroast.ce742601.JPG"
+  },
+  {
+    "revision": "aaf6c7ec725ace1a64376c298571ea36",
+    "url": "/food-recipe/static/media/honeychicken.aaf6c7ec.JPG"
+  },
+  {
+    "revision": "43efba8de1740231ffa11c635898e754",
+    "url": "/food-recipe/static/media/italian.43efba8d.jpg"
+  },
+  {
+    "revision": "c9d851cca4c1456ca38cb775592b3ce9",
+    "url": "/food-recipe/static/media/kachori.c9d851cc.JPG"
+  },
+  {
+    "revision": "90c41e12b6f9631e5ba6eb1a0523874a",
+    "url": "/food-recipe/static/media/manchurian.90c41e12.JPG"
+  },
+  {
+    "revision": "eac55e0c909e92a45c6ab1dbbde3a047",
+    "url": "/food-recipe/static/media/masalvada.eac55e0c.JPG"
+  },
+  {
+    "revision": "c1d3af0b6e9d5c77bd1efc30b8c34f6d",
+    "url": "/food-recipe/static/media/momos.c1d3af0b.JPG"
+  },
+  {
+    "revision": "f9b71338d30250d98893dec0bbe16c63",
+    "url": "/food-recipe/static/media/mushroom.f9b71338.JPG"
+  },
+  {
+    "revision": "2f840d7f62956d2a4412127f25b14b4c",
+    "url": "/food-recipe/static/media/noodles.2f840d7f.JPG"
+  },
+  {
+    "revision": "1684e71ea957dd045f1217424d04a6e9",
+    "url": "/food-recipe/static/media/northindian (1).1684e71e.jpg"
+  },
+  {
+    "revision": "2e0a6c743a3cc42f3d5eadf9454e536e",
+    "url": "/food-recipe/static/media/panner.2e0a6c74.JPG"
+  },
+  {
+    "revision": "a66cbb3df20b78067048d60f8ca4cdcf",
+    "url": "/food-recipe/static/media/pasta.a66cbb3d.JPG"
+  },
+  {
+    "revision": "66a6338cd40fe670efec2b391641ea31",
+    "url": "/food-recipe/static/media/pizza.66a6338c.JPG"
+  },
+  {
+    "revision": "9f82335bb3db8754463e1c1b338a05de",
+    "url": "/food-recipe/static/media/southindian.9f82335b.jpg"
+  }
+]);
